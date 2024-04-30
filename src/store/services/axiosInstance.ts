a5593485247bd6ffb20129b81b274be0a69bc454
solomiia-marko <soliamark25@gmail.com>
2024-04-30 19:19:47 +0300

    chore: fixed baseUrl in axios

--- src/store/services/axiosInstance.ts
+++ src/store/services/axiosInstance.ts
@@ -1,9 +1,4 @@
 import axios from 'axios';
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
+const axiosInstance = axios.create({});
 
 export default axiosInstance;
