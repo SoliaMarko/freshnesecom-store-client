b7d9d90393df304973e7b58de65c4d47034254a1
solomiia-marko <soliamark25@gmail.com>
2024-05-02 20:45:01 +0300

    chore: removed redundant baseUrl from rtk

--- src/store/services/authApi.ts
+++ src/store/services/authApi.ts
@@ -4,9 +4,7 @@ import {axiosBaseQuery} from './axiosBaseQuery';
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: axiosBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL
-  }),
+  baseQuery: axiosBaseQuery(),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (body: SignUpFormType) => {
