3f3d433afbc7321ec94f92213633bdf457655a00
solomiia-marko <soliamark25@gmail.com>
2024-06-07 18:10:45 +0300

    chore: fixed vite.config.ts env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
+const env = loadEnv('development', process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
